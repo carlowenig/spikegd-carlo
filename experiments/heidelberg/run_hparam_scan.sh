4098927563dc3ab0141417e836191653f838b1b5
Carlo Wenig <carlo@hhnc-wenig.de>
2024-09-03 15:58:56 +0200

    modify hparam scanning for marvin (2)

---
+++ experiments/heidelberg/run_hparam_scan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --partition=A40devel
+#SBATCH --time=0:00:30
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+
+module load Python/3.12
+source ../../.venv/bin/activate
+python hparam_scan.py
